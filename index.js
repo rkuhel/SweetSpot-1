b8fadc8ad85e734c7b48f6cc120eb59132153c0c
Dmitriy A <dmitriy.a@gmail.com>
2014-05-10 15:07:07 -0400

    wokring

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 var userKey = "016ff052ff52252d55547e9c73db743a"
 $(document).ready(function() {
-  // $.getJSON("http://query.yahooapis.com/v1/public/yql", {
-  //     q: "select * from json where url=\"http://api.crunchbase.com/v/2/organizations?user_key=" + userKey + "&page=1&order=updated_at+DESC\"",
-  //     //callback: gotJSON, // you don't even need this line if your browser supports CORS
-  //     format: "json"
-  //   },
-  //   function(data) {
-  //     if (data.query.results) {
-  //       console.log(data.query.results.json.data);
-  //       $.each(data.query.results.json.data.items, function(index, value) {
-  //         var companyPath = value.path.split("/")[1]
-  //         getFundingRoundUUID(companyPath, {
-  //           companyName: value.name,
-  //           fundingRounds:[]
-  //         });
-  //         //$('div.startups').append("<div>"+value.name+"</div>")
-  //         return index < 50;
-  //       })
-  //     } else {
-  //       console.log("nothing")
-  //     }
-  //   }
-  // );
-  getFundingRoundUUID("hublished", {
-    companyName: "hublished",
-    fundingRounds: []
-  })
+  $.getJSON("http://query.yahooapis.com/v1/public/yql", {
+      q: "select * from json where url=\"http://api.crunchbase.com/v/2/organizations?user_key=" + userKey + "&page=1&order=updated_at+DESC\"",
+      //callback: gotJSON, // you don't even need this line if your browser supports CORS
+      format: "json"
+    },
+    function(data) {
+      if (data.query.results) {
+        console.log(data.query.results.json.data);
+        $.each(data.query.results.json.data.items, function(index, value) {
+          var companyPath = value.path.split("/")[1]
+          getFundingRoundUUID(companyPath, {
+            companyName: value.name,
+            fundingRounds:[]
+          });
+          //$('div.startups').append("<div>"+value.name+"</div>")
+          return index < 100;
+        })
+      } else {
+        console.log("nothing")
+      }
+    }
+  );
+  // getFundingRoundUUID("fondu", {
+  //   companyName: "fondu",
+  //   fundingRounds: []
+  // })
 });
 
 function getFundingRoundUUID(path, companyObject) {
@@ -35,7 +35,7 @@ function getFundingRoundUUID(path, companyObject) {
       //callback: gotJSON, // you don't even need this line if your browser supports CORS
       format: "json"
     },
-    function(data,status) {
+    function(data, status) {
       if (data.query.results) {
         console.log(data.query.results.json.data);
         if ("funding_rounds" in data.query.results.json.data.relationships) {
@@ -45,7 +45,7 @@ function getFundingRoundUUID(path, companyObject) {
               var uuid = value.path.split("/")[1];
               var fundingRound = getFundingRound(uuid);
               deferred.push(fundingRound)
-              fundingRound.then(function(round){
+              fundingRound.then(function(round) {
                 companyObject.fundingRounds.push(round);
               })
               //deferred.push(fundingRound);
@@ -57,10 +57,11 @@ function getFundingRoundUUID(path, companyObject) {
             })
           } else {
             var uuid = data.query.results.json.data.relationships.funding_rounds.items.path.split("/")[1]
-            $.when(getFundingRound(uuid)).then(
-              function(fundingRound, status) {
-                companyObject.fundingRounds.push(fundingRound);
-              });
+            var fundingRound = getFundingRound(uuid);
+            deferred.push(fundingRound)
+            fundingRound.then(function(round) {
+              companyObject.fundingRounds.push(round);
+            })
           }
           $.when.apply($, deferred).then(function(fundingRound) {
             console.log("showing")
@@ -83,7 +84,7 @@ function getFundingRound(uuid) {
     //callback: gotJSON, // you don't even need this line if your browser supports CORS
     format: "json"
   });
-  defer.success(function(data,status) {
+  defer.success(function(data, status) {
     if (data.query.results) {
       fundingRound = {
         moneyRaised: null,
@@ -115,24 +116,24 @@ function filterAndShow(companyObject) {
   var totalFunding = 0;
   var indexOfTop = -1;
   for (var i = 0; i < companyObject.fundingRounds.length; i++) {
-    if(companyObject.fundingRounds[i].moneyRaised != null){
-      if(indexOfTop == -1) {
+    if (companyObject.fundingRounds[i].moneyRaised != null) {
+      if (indexOfTop == -1) {
         indexOfTop = i;
       }
       totalFunding += parseInt(companyObject.fundingRounds[i].moneyRaised);
-      if(parseInt(companyObject.fundingRounds[i].moneyRaised)> parseInt(companyObject.fundingRounds[indexOfTop].moneyRaised)){
+      if (parseInt(companyObject.fundingRounds[i].moneyRaised) > parseInt(companyObject.fundingRounds[indexOfTop].moneyRaised)) {
         indexOfTop = i;
       }
     }
     if (!(companyObject.fundingRounds[i].announcedDate == "2011" || companyObject.fundingRounds[i].announcedDate == "2012" ||
       companyObject.fundingRounds[i].announcedDate == "2013" || companyObject.fundingRounds[i].announcedDate == "2014")) {
-      console.log("returning false" )
+      console.log("returning false")
       return false;
     }
   }
   console.log(totalFunding)
   console.log(indexOfTop)
-  if(indexOfTop == -1) {
+  if (indexOfTop == -1) {
     return false;
   }
   if (totalFunding > 100000 && totalFunding < 1000000) {
